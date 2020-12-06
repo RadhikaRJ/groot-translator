31926dea44c583d628445934153b372e4437683c
RadhikaRJ <radhika7002@gmail.com>
2020-12-06 21:39:22 +0530

    basic design completed & serverURL added to js

---
+++ app.js
@@ -0,0 +1,11 @@
+var btntranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#input-text");
+var outputDv = document.querySelector(".output");
+btntranslate.addEventListener("click",clickEventHandler);
+
+var serverURL = "https://api.funtranslations.com/translate/groot.json";
+
+
+function clickEventHandler(){
+
+}

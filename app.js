6486d9bf2fe21aab384c8005bf889eb05bee3cb1
RadhikaRJ <radhika7002@gmail.com>
2020-12-06 22:02:14 +0530

    implement clickeventhandler & check functionality

--- app.js
+++ app.js
@@ -5,7 +5,25 @@ btntranslate.addEventListener("click",clickEventHandler);
 
 var serverURL = "https://api.funtranslations.com/translate/groot.json";
 
+function createURL(text)
+{
+return serverURL+"?"+"text="+text;
+}
+
+function errorHandler(error){
+    console.log("There's an error: ",error);
+    alert("Server is busy, Try later!");
+}
 
 function clickEventHandler(){
+    var userEnteredText = inputText.value;
+
+    fetch(createURL(userEnteredText))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText = json.contents.translated;
+        outputDv.innerHTML = translatedText;
+    })
+    .catch(errorHandler);
 
 }
